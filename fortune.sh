c75f485dfdc0ea50c9ca48a5949520a869c63917
ALexandru <alexneguleanu@gmail.com>
2024-07-04 17:56:08 +0300

    Added some comments

--- fortune.sh
+++ fortune.sh
@@ -2,15 +2,17 @@
 
 # Program to tell a persons fortune
 
+# Title
 echo -e "\n~~ Fortune Teller ~~\n"
 
+# Array o responses
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
-
+# Random var btw 0 and 5
 N=$(( RANDOM % 6 ))
 
+# Function
 GET_FORTUNE() {
-  # echo "Ask a yes or no question:"
   if [[ ! $1 ]]
     then 
       echo "Ask a yes or no question:"
@@ -20,14 +22,18 @@ GET_FORTUNE() {
   read QUESTION
 }  
 
+# Function call
 GET_FORTUNE
 
+# Until loop
 until [[ $QUESTION =~ \?$ ]]
 do
   GET_FORTUNE again
 done
 
+# Print the response
 echo -e "\n${RESPONSES[$N]}"
+
 
 : ' 
   until [[ CONDITION ]]
